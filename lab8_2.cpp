254bd3c4b66997358f02f9e4bfe91d6cb7cd5e96
teamlab8 <anuwatch6552@gmail.com>
2022-12-20 15:52:28 +0700

    tea

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,34 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include<string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+    int num;
+    string day;
+    string sy;
+    string mov;
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+    getline(cin,name);
+    cout << "?????: ";
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << "\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << "\n";
+    cin >> num;
+    cin.ignore();
+    cout << name << ": ";
+    string str = to_string(num);
+    string first = str.substr(0,2);
+    int firt_int = stoi(first);
+    cout << "Fahsai: I think you may be GEAR " << firt_int - 12 << ". I have a free movie ticket for you." << "\n";
+    cout << "Fahsai: Let's go to the cinema together!!!" << "\n";
+    cout << "Fahsai: What movie do you want to watch?" << "\n";
+    getline(cin,mov);
+    cout << name << ": ";
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    getline(cin,day);
+    cout << name << ": ";
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << mov << " with you.\n";
+    getline(cin,sy);
+    cout << name << ": ";
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+}
